461495e464757bfde1576bf57916710f7d7a02b1
leothawne <leothawne@gmail.com>
2018-11-10 23:26:05 -0200

    Vault integration and security measures added

--- src/main/java/io/github/leothawne/LTItemMail/LTItemMailLoader.java
+++ src/main/java/io/github/leothawne/LTItemMail/LTItemMailLoader.java
@@ -16,9 +16,11 @@ import io.github.leothawne.LTItemMail.commands.constructor.SendBoxConstructTabCo
 import io.github.leothawne.LTItemMail.commands.inventories.events.OpenBoxInventoryEvent;
 import io.github.leothawne.LTItemMail.commands.inventories.events.SendBoxInventoryEvent;
 import io.github.leothawne.LTItemMail.player.Listeners;
+import net.milkbowl.vault.economy.Economy;
 
 public class LTItemMailLoader extends JavaPlugin {
 	private ConsoleLoader myLogger = new ConsoleLoader(this);
+	private Economy economyPlugin = null;
 	public static void registerEvents(LTItemMailLoader plugin, Listener...listeners) {
 		for(Listener listener : listeners) {
 			Bukkit.getServer().getPluginManager().registerEvents(listener, plugin);
@@ -32,25 +34,38 @@ public class LTItemMailLoader extends JavaPlugin {
 		}
 		myLogger.Hello();
 		myLogger.info("Loading mailboxes...");
-		new ConfigurationLoader(this, myLogger).check();
-		configuration = new ConfigurationLoader(this, myLogger).load();
-		if(configuration.getBoolean("enable-plugin") == true) {
-			getCommand("itemmail").setExecutor(new ItemMailCommands(this, myLogger));
-			getCommand("itemmail").setTabCompleter(new ItemMailConstructTabCompleter());
-			getCommand("itemmailadmin").setExecutor(new ItemMailAdminCommands(this, myLogger));
-			getCommand("itemmailadmin").setTabCompleter(new ItemMailAdminConstructTabCompleter());
-			getCommand("sendbox").setExecutor(new SendBoxCommand(this, myLogger));
-			getCommand("sendbox").setTabCompleter(new SendBoxConstructTabCompleter());
-			registerEvents(this, new SendBoxInventoryEvent(this));
-			registerEvents(this, new OpenBoxInventoryEvent());
-			registerEvents(this, new Listeners(configuration));
-			new Version(this, myLogger).check();
-			myLogger.warning("A permissions plugin is required! Just make sure you are using one. Permissions nodes can be found at: https://leothawne.github.io/LTItemMail/permissions.html");
-			for(Player player : this.getServer().getOnlinePlayers()) {
-				player.sendMessage(ChatColor.AQUA + "[LTItemMail] " + ChatColor.LIGHT_PURPLE + "Loaded!");
+		myLogger.info("Loading Vault...");
+		VaultLoader vault = new VaultLoader(this);
+		if(vault.isInstalled()) {
+			if(vault.isReady()) {
+				economyPlugin = vault.getEconomy();
+				new ConfigurationLoader(this, myLogger).check();
+				configuration = new ConfigurationLoader(this, myLogger).load();
+				if(configuration.getBoolean("enable-plugin") == true) {
+					getCommand("itemmail").setExecutor(new ItemMailCommands(this, myLogger));
+					getCommand("itemmail").setTabCompleter(new ItemMailConstructTabCompleter());
+					getCommand("itemmailadmin").setExecutor(new ItemMailAdminCommands(this, myLogger));
+					getCommand("itemmailadmin").setTabCompleter(new ItemMailAdminConstructTabCompleter());
+					getCommand("sendbox").setExecutor(new SendBoxCommand(this, myLogger));
+					getCommand("sendbox").setTabCompleter(new SendBoxConstructTabCompleter());
+					registerEvents(this, new SendBoxInventoryEvent(this, economyPlugin));
+					registerEvents(this, new OpenBoxInventoryEvent());
+					registerEvents(this, new Listeners(configuration));
+					new Version(this, myLogger).check();
+					myLogger.warning("A permissions plugin is required! Just make sure you are using one. Permissions nodes can be found at: https://leothawne.github.io/LTItemMail/permissions.html");
+					for(Player player : this.getServer().getOnlinePlayers()) {
+						player.sendMessage(ChatColor.AQUA + "[LTItemMail] " + ChatColor.LIGHT_PURPLE + "Loaded!");
+					}
+				} else {
+					myLogger.severe("You manually choose to disable this plugin.");
+					getServer().getPluginManager().disablePlugin(this);
+				}
+			} else {
+				myLogger.severe("An economy plugin is missing! Disabling LT Item Mail...");
+				getServer().getPluginManager().disablePlugin(this);
 			}
 		} else {
-			myLogger.severe("You manually choose to disable this plugin.");
+			myLogger.severe("Vault is missing! Disabling LT Item Mail...");
 			getServer().getPluginManager().disablePlugin(this);
 		}
 	}
--- src/main/java/io/github/leothawne/LTItemMail/commands/inventories/events/SendBoxInventoryEvent.java
+++ src/main/java/io/github/leothawne/LTItemMail/commands/inventories/events/SendBoxInventoryEvent.java
@@ -15,11 +15,15 @@ import org.bukkit.inventory.ItemStack;
 import io.github.leothawne.LTItemMail.LTItemMailLoader;
 import io.github.leothawne.LTItemMail.commands.inventories.OpenBoxInventory;
 import io.github.leothawne.LTItemMail.commands.inventories.SendBoxInventory;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
 
 public class SendBoxInventoryEvent implements Listener {
 	private LTItemMailLoader plugin;
-	public SendBoxInventoryEvent(LTItemMailLoader plugin){
+	private Economy economyPlugin;
+	public SendBoxInventoryEvent(LTItemMailLoader plugin, Economy economyPlugin){
 		this.plugin = plugin;
+		this.economyPlugin = economyPlugin;
 	}
 	private String inventoryName = new SendBoxInventory().getName();
 	@EventHandler
@@ -29,14 +33,25 @@ public class SendBoxInventoryEvent implements Listener {
 			Player sender = (Player) event.getPlayer();
 			Player recipient = (Player) inventory.getHolder();
 			ItemStack[] contents = inventory.getContents();
+			ItemStack[] recipientContents = recipient.getInventory().getContents();
 			List<ItemStack> contentsarray = new ArrayList<ItemStack>();
+			int countslot = 0;
 			for(ItemStack content : contents) {
 				if(content == null) {
 					contentsarray.add(new ItemStack(Material.AIR));
+					countslot++;
 				} else {
 					contentsarray.add(content);
 				}
 			}
+			int slots = 54 - countslot;
+			int countrecipientslot = 0;
+			for(ItemStack content : recipientContents) {
+				if(content != null) {
+					countrecipientslot++;
+				}
+			}
+			int recipientslots = 36 - countrecipientslot;
 			boolean isEmpty = true;
 			for(ItemStack content : contentsarray) {
 				if(content.getType() != Material.AIR) {
@@ -45,20 +60,46 @@ public class SendBoxInventoryEvent implements Listener {
 			}
 			inventory.clear();
 			if(isEmpty == false) {
-				if(recipient.isInvulnerable() == false) {
-					sender.sendMessage(ChatColor.DARK_GREEN + "[LTIM] " + ChatColor.YELLOW + "Items sent to " + ChatColor.AQUA + "" + recipient.getName() + "" + ChatColor.YELLOW + "!");
-					recipient.setInvulnerable(true);
-					recipient.sendMessage(ChatColor.DARK_GREEN + "[LTIM] " + ChatColor.AQUA + "Mailbox from: " + ChatColor.GREEN + "" + sender.getName() + "" + ChatColor.AQUA + ".");
-					recipient.sendMessage(ChatColor.DARK_GREEN + "[LTIM] " + ChatColor.AQUA + "Opening in " + ChatColor.GREEN + "5 seconds" + ChatColor.AQUA + "... " + ChatColor.DARK_RED + "Items left inside will be lost!");
-					recipient.sendTitle(ChatColor.AQUA + "Mailbox from: " + ChatColor.GREEN + "" + sender.getName(), ChatColor.AQUA + "Opening in " + ChatColor.GREEN + "5 seconds" + ChatColor.AQUA + "... " + ChatColor.DARK_RED + "Items left inside will be lost!", 20 * 1, 20 * 5, 20 * 1);
-					plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-						public void run() {
-							recipient.openInventory(new OpenBoxInventory().GUI(contentsarray));
-							sender.sendMessage(ChatColor.DARK_GREEN + "[LTIM] " + ChatColor.YELLOW + "Items delivered successfully!");
+				if(recipientslots >= slots) {
+					if(recipient.isInvulnerable() == false) {
+						if(economyPlugin.has(sender, 10.0)) {
+							EconomyResponse er = economyPlugin.withdrawPlayer(sender, 10.0);
+							if(er.transactionSuccess()) {
+								sender.sendMessage(ChatColor.DARK_GREEN + "[LTIM] " + ChatColor.YELLOW + "You paid " + ChatColor.GREEN + "$10.0 " + ChatColor.YELLOW + "to the bank! " + recipientslots);
+								sender.sendMessage(ChatColor.DARK_GREEN + "[LTIM] " + ChatColor.YELLOW + "Items sent to " + ChatColor.AQUA + "" + recipient.getName() + "" + ChatColor.YELLOW + ". " + slots);
+								recipient.setInvulnerable(true);
+								recipient.sendMessage(ChatColor.DARK_GREEN + "[LTIM] " + ChatColor.AQUA + "Mailbox from: " + ChatColor.GREEN + "" + sender.getName() + "" + ChatColor.AQUA + ".");
+								recipient.sendMessage(ChatColor.DARK_GREEN + "[LTIM] " + ChatColor.AQUA + "Opening in " + ChatColor.GREEN + "5 seconds" + ChatColor.AQUA + "... " + ChatColor.DARK_RED + "Items left inside will be lost!");
+								recipient.sendTitle(ChatColor.AQUA + "Mailbox from: " + ChatColor.GREEN + "" + sender.getName(), ChatColor.AQUA + "Opening in " + ChatColor.GREEN + "5 seconds" + ChatColor.AQUA + "... " + ChatColor.DARK_RED + "Items left inside will be lost!", 20 * 1, 20 * 5, 20 * 1);
+								plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+									public void run() {
+										recipient.openInventory(new OpenBoxInventory().GUI(contentsarray));
+										sender.sendMessage(ChatColor.DARK_GREEN + "[LTIM] " + ChatColor.YELLOW + "Items delivered successfully!");
+									}
+								}, 20 * 7);
+							} else {
+								sender.sendMessage(ChatColor.DARK_GREEN + "[LTIM] " + ChatColor.YELLOW + "Transaction not succeeded!");
+								for(ItemStack item : contentsarray) {
+									sender.getInventory().addItem(item);
+								}
+							}
+						} else {
+							sender.sendMessage(ChatColor.DARK_GREEN + "[LTIM] " + ChatColor.YELLOW + "You don't have " + ChatColor.GREEN + "$10.0 " + ChatColor.YELLOW + "to pay.");
+							for(ItemStack item : contentsarray) {
+								sender.getInventory().addItem(item);
+							}
 						}
-					}, 20 * 7);
+					} else {
+						sender.sendMessage(ChatColor.DARK_GREEN + "[LTIM] " + ChatColor.AQUA + "" + recipient.getName() + "" + ChatColor.YELLOW + " is busy right now! Try again later.");
+						for(ItemStack item : contentsarray) {
+							sender.getInventory().addItem(item);
+						}
+					}
 				} else {
-					sender.sendMessage(ChatColor.DARK_GREEN + "[LTIM] " + ChatColor.AQUA + "" + recipient.getName() + "" + ChatColor.YELLOW + " is busy right now! Try again later.");
+					sender.sendMessage(ChatColor.DARK_GREEN + "[LTIM] " + ChatColor.AQUA + "" + recipient.getName() + "" + ChatColor.YELLOW + " doesn't have " + slots + " slots available.");
+					for(ItemStack item : contentsarray) {
+						sender.getInventory().addItem(item);
+					}
 				}
 			} else {
 				sender.sendMessage(ChatColor.DARK_GREEN + "[LTIM] " + ChatColor.YELLOW + "Box empty! Aborted.");
