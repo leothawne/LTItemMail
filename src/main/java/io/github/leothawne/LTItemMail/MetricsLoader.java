60dc027dca24d0764c6557a8556e06f1f482310f
leothawne <leothawne@gmail.com>
2018-11-27 19:55:12 -0200

    Added console messages

--- src/main/java/io/github/leothawne/LTItemMail/MetricsLoader.java
+++ src/main/java/io/github/leothawne/LTItemMail/MetricsLoader.java
@@ -11,8 +11,10 @@ public class MetricsLoader {
 	}
 	public void init() {
 		Metrics metrics = new Metrics(plugin);
-		if(metrics.isEnabled() == false) {
-			myLogger.warning("LT Item Mail is using bStats. Make sure bStats is enabled! Thank you.");
+		if(metrics.isEnabled() == true) {
+			myLogger.info("LT Item Mail is using bStats to collect data to improve the next versions. In case you want to know what data will be collected: https://bstats.org/getting-started");
+		} else {
+			myLogger.warning("bStats is disabled and LT Item Mail cannot use it. Please enable bStats! Thank you.");
 		}
 	}
 }
