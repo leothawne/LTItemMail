1fc9806a7feca1500c2624be383f74830bca9926
BuildTools <31146142+leothawne@users.noreply.github.com>
2024-05-12 04:15:20 -0300

    Added Minecraft username cache support

--- src/main/java/io/github/leothawne/LTItemMail/lib/Fetch.java
+++ src/main/java/io/github/leothawne/LTItemMail/lib/Fetch.java
@@ -1,25 +1,64 @@
 package io.github.leothawne.LTItemMail.lib;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import io.github.leothawne.LTItemMail.module.ConfigurationModule;
 
 public final class Fetch {
 	private Fetch() {}
-	public static final String get(final String url) {
-		try {
-			final URLConnection connection = URI.create(url).toURL().openConnection();
-			connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36");
-			connection.setUseCaches(false);
-			connection.connect();
-			final BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("UTF-8")));
-			final StringBuilder builder = new StringBuilder();
-			String string;
-			while((string = reader.readLine()) != null) builder.append(string);
-			return builder.toString();
-		} catch(final Exception exception) {
+	public static final class URL {
+		public static final String get(final String url) {
+			try {
+				final URLConnection connection = URI.create(url).toURL().openConnection();
+				connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36");
+				connection.setUseCaches(false);
+				connection.connect();
+				final BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("UTF-8")));
+				final StringBuilder builder = new StringBuilder();
+				String string;
+				while((string = reader.readLine()) != null) builder.append(string);
+				return builder.toString();
+			} catch(final Exception exception) {
+				return null;
+			}
+		}
+	}
+	public static final class Player {
+		private static final JSONArray array() {
+			final File cache = new File(Bukkit.getWorldContainer().getAbsolutePath(), "usercache.json");
+			try {
+				return (JSONArray) new JSONParser().parse(new FileReader(cache));
+			} catch (final IOException | ParseException e) {
+				if((Boolean) ConfigurationModule.get(ConfigurationModule.Type.PLUGIN_DEBUG)) e.printStackTrace();
+			}
+			return null;
+		}
+		public static final UUID fromName(final String name) {
+			if(array() != null) for(int i = 0; i < array().size(); i++) {
+				final JSONObject user = (JSONObject) array().get(i);
+				if(name.equalsIgnoreCase((String) user.get("name"))) return UUID.fromString((String) user.get("uuid"));
+			}
+			return null;
+		}
+		public static final String fromUUID(final UUID uuid) {
+			if(array() != null) for(int i = 0; i < array().size(); i++) {
+				final JSONObject user = (JSONObject) array().get(i);
+				if(uuid.equals(UUID.fromString((String) user.get("uuid")))) return (String) user.get("name");
+			}
 			return null;
 		}
 	}
