7439e65f500d588c6376076f61855033774f17b1
BuildTools <31146142+leothawne@users.noreply.github.com>
2024-05-12 04:14:58 -0300

    Update Completer.java

--- src/main/java/io/github/leothawne/LTItemMail/lib/Completer.java
+++ src/main/java/io/github/leothawne/LTItemMail/lib/Completer.java
@@ -2,16 +2,15 @@ package io.github.leothawne.LTItemMail.lib;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.bukkit.util.StringUtil;
 
 public final class Completer {
 	private Completer() {}
-	public static final ArrayList<String> partial(final String token, final Collection<String> from) {
-		final ArrayList<String> strings = new ArrayList<String>();
-		for(final Object object : StringUtil.copyPartialMatches(token, from, new ArrayList<>(from.size()))) {
-			strings.add(object.toString());
-		}
-        return strings;
+	public static final List<String> partial(final String token, final Collection<String> from) {
+		final List<String> response = new ArrayList<String>();
+		for(final Object object : StringUtil.copyPartialMatches(token, from, new ArrayList<>(from.size()))) response.add(object.toString());
+        return response;
     }
 }
