106c545c35745fb4ea01c48b8df7b414dc9bd003
BuildTools <31146142+leothawne@users.noreply.github.com>
2024-05-12 04:13:26 -0300

    Added Bungee support

--- src/main/java/io/github/leothawne/LTItemMail/command/tabCompleter/ItemMailAdminCommandTabCompleter.java
+++ src/main/java/io/github/leothawne/LTItemMail/command/tabCompleter/ItemMailAdminCommandTabCompleter.java
@@ -13,14 +13,19 @@ import org.bukkit.entity.Player;
 import com.google.common.collect.ImmutableList;
 
 import io.github.leothawne.LTItemMail.lib.Completer;
+import io.github.leothawne.LTItemMail.module.BungeeModule;
+import io.github.leothawne.LTItemMail.module.ConfigurationModule;
 import io.github.leothawne.LTItemMail.module.PermissionModule;
 
 public final class ItemMailAdminCommandTabCompleter implements TabCompleter {
 	@Override
 	public final List<String> onTabComplete(final CommandSender sender, Command cmd, final String commandLabel, final String[] args){
-		if(args.length == 1) if(PermissionModule.hasPermission(sender, PermissionModule.Type.CMD_ADMIN_MAIN)) return Completer.partial(args[0], ImmutableList.of("update", "list", "recover", "reload"));
-		if(args.length == 2) if(PermissionModule.hasPermission(sender, PermissionModule.Type.CMD_ADMIN_LIST)) if(args[0].equals("list")) {
+		if(args.length == 1) if(PermissionModule.hasPermission(sender, PermissionModule.Type.CMD_ADMIN_MAIN)) return Completer.partial(args[0], ImmutableList.of("update", "list", "recover", "reload", "ban", "unban", "banlist"));
+		if(args.length == 2) if(PermissionModule.hasPermission(sender, PermissionModule.Type.CMD_ADMIN_LIST)) if(args[0].equals("list") || args[0].equals("ban") || args[0].equals("unban")) {
 			final LinkedList<String> response = new LinkedList<>();
+			if((Boolean) ConfigurationModule.get(ConfigurationModule.Type.BUNGEE_MODE)) {
+				for(final String bungeePlayer : BungeeModule.getOnlinePlayers()) response.add(bungeePlayer);
+			} else for(final Player onlinePlayer : Bukkit.getOnlinePlayers()) response.add(onlinePlayer.getName());
 			for(final Player p: Bukkit.getOnlinePlayers()) response.add(p.getName());
 			return Completer.partial(args[1], response);
 		}
--- src/main/java/io/github/leothawne/LTItemMail/command/tabCompleter/MailItemCommandTabCompleter.java
+++ src/main/java/io/github/leothawne/LTItemMail/command/tabCompleter/MailItemCommandTabCompleter.java
@@ -11,15 +11,27 @@ import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
 import io.github.leothawne.LTItemMail.lib.Completer;
+import io.github.leothawne.LTItemMail.module.BungeeModule;
+import io.github.leothawne.LTItemMail.module.ConfigurationModule;
 import io.github.leothawne.LTItemMail.module.PermissionModule;
 
 public final class MailItemCommandTabCompleter implements TabCompleter {
 	public MailItemCommandTabCompleter() {}
 	@Override
 	public final List<String> onTabComplete(final CommandSender sender, final Command cmd, final String commandLabel, final String[] args){
+		Player player = null;
 		if(args.length == 1) if(PermissionModule.hasPermission(sender, PermissionModule.Type.CMD_PLAYER_SEND)) {
+			if(sender instanceof Player) player = (Player) sender;
 			final LinkedList<String> response = new LinkedList<>();
-			for(final Player player : Bukkit.getOnlinePlayers()) response.add(player.getName());
+			if((Boolean) ConfigurationModule.get(ConfigurationModule.Type.BUNGEE_MODE)) {
+				for(final String bungeePlayer : BungeeModule.getOnlinePlayers()) {
+					if(bungeePlayer.equals(player.getName())) continue;
+					response.add(bungeePlayer);
+				}
+			} else for(final Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+				if(onlinePlayer.getName().equals(player.getName())) continue;
+				response.add(onlinePlayer.getName());
+			}
 			return Completer.partial(args[0], response);
 		}
 		return Collections.emptyList();
