9760ff72f68256789396aad811d339c581696603
BuildTools <31146142+leothawne@users.noreply.github.com>
2024-05-12 04:14:27 -0300

    Update PlayerSendMailEvent.java

--- src/main/java/io/github/leothawne/LTItemMail/event/PlayerSendMailEvent.java
+++ src/main/java/io/github/leothawne/LTItemMail/event/PlayerSendMailEvent.java
@@ -2,22 +2,29 @@ package io.github.leothawne.LTItemMail.event;
 
 import java.util.LinkedList;
 
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
 
+import io.github.leothawne.LTItemMail.LTPlayer;
+/**
+ * 
+ * This event is called when a mailbox is sent.
+ * 
+ * @author leothawne
+ * 
+ */
 public final class PlayerSendMailEvent extends Event implements Cancellable {
 	private final CommandSender from;
-	private final OfflinePlayer playerTo;
+	private final LTPlayer playerTo;
 	private final LinkedList<ItemStack> contents;
 	private final boolean hasCost;
 	private final double cost;
 	private static final HandlerList handlers = new HandlerList();
 	private boolean cancelled;
-	public PlayerSendMailEvent(final CommandSender from, final OfflinePlayer playerTo, final LinkedList<ItemStack> contents, final boolean hasCost, final double cost) {
+	public PlayerSendMailEvent(final CommandSender from, final LTPlayer playerTo, final LinkedList<ItemStack> contents, final boolean hasCost, final double cost) {
 		this.from = from;
 		this.playerTo = playerTo;
 		this.contents = contents;
@@ -25,18 +32,43 @@ public final class PlayerSendMailEvent extends Event implements Cancellable {
 		this.cost = cost;
 		cancelled = false;
 	}
-	public final CommandSender getPlayerFrom() {
+	/**
+	 * 
+	 * Gets who sent the mailbox.
+	 * 
+	 */
+	public final CommandSender getFrom() {
 		return from;
 	}
-	public final OfflinePlayer getPlayerTo() {
+	/**
+	 * 
+	 * Gets who received the mailbox.
+	 * 
+	 */
+	public final LTPlayer getPlayerTo() {
 		return playerTo;
 	}
+	/**
+	 * 
+	 * Gets the mailbox contents.
+	 * 
+	 */
 	public final LinkedList<ItemStack> getContents(){
 		return contents;
 	}
+	/**
+	 * 
+	 * Returns "true" if the mailbox was paid to be sent.
+	 * 
+	 */
 	public final boolean hasCost() {
 		return hasCost;
 	}
+	/**
+	 * 
+	 * If the mailbox was paid, returns the paid value, otherwise it will return "0.0".
+	 * 
+	 */
 	public final double cost() {
 		return cost;
 	}
@@ -47,10 +79,20 @@ public final class PlayerSendMailEvent extends Event implements Cancellable {
 	public static final HandlerList getHandlerList() {
 		return handlers;
 	}
+	/**
+	 * 
+	 * Gets if the event was cancelled.
+	 * 
+	 */
 	@Override
 	public final boolean isCancelled() {
 		return cancelled;
 	}
+	/**
+	 * 
+	 * Cancels the event. If cancelled, mailbox deliver is stopped before it reaches the addressee.
+	 * 
+	 */
 	@Override
 	public final void setCancelled(final boolean cancel) {
 		cancelled = cancel;
