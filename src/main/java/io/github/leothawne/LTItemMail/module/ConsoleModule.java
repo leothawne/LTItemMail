a660df7d132bfeadd4dec6a38a84518086072305
BuildTools <31146142+leothawne@users.noreply.github.com>
2024-06-17 03:56:12 -0300

    Update ConsoleModule.java

--- src/main/java/io/github/leothawne/LTItemMail/module/ConsoleModule.java
+++ src/main/java/io/github/leothawne/LTItemMail/module/ConsoleModule.java
@@ -12,52 +12,52 @@ import io.github.leothawne.LTItemMail.LTItemMail;
 
 public final class ConsoleModule {
 	private ConsoleModule() {}
-	private static final ConsoleCommandSender getConsoleSender() {
+	private static final ConsoleCommandSender sender() {
 		return Bukkit.getConsoleSender();
 	}
 	public static final void hello() {
-		getConsoleSender().sendMessage(ChatColor.DARK_AQUA + " _   _______ _____ __  __ ");
-		getConsoleSender().sendMessage(ChatColor.DARK_AQUA + "| | |__   __|_   _|  \\/  |");
-		getConsoleSender().sendMessage(ChatColor.DARK_AQUA + "| |    | |    | | | \\  / |" + ChatColor.WHITE + "  Build number: " + LTItemMail.getInstance().getDescription().getVersion());
-		getConsoleSender().sendMessage(ChatColor.DARK_AQUA + "| |    | |    | | | |\\/| |" + ChatColor.WHITE + "  Build date & time: " + DataModule.getVersionDate());
-		getConsoleSender().sendMessage(ChatColor.DARK_AQUA + "| |____| |   _| |_| |  | |" + ChatColor.WHITE + "  Requires: Java " + DataModule.getVersion(DataModule.VersionType.JAVA) + " (or higher)");
-		getConsoleSender().sendMessage(ChatColor.DARK_AQUA + "|______|_|  |_____|_|  |_|" + ChatColor.WHITE + "  Need support or have questions? https://discord.gg/Nvnrv3P");
+		sender().sendMessage(ChatColor.DARK_AQUA + " _   _______ _____ __  __ ");
+		sender().sendMessage(ChatColor.DARK_AQUA + "| | |__   __|_   _|  \\/  |");
+		sender().sendMessage(ChatColor.DARK_AQUA + "| |    | |    | | | \\  / |" + ChatColor.WHITE + "  Build number: " + LTItemMail.getInstance().getDescription().getVersion());
+		sender().sendMessage(ChatColor.DARK_AQUA + "| |    | |    | | | |\\/| |" + ChatColor.WHITE + "  Build date/time: " + DataModule.getDate());
+		sender().sendMessage(ChatColor.DARK_AQUA + "| |____| |   _| |_| |  | |" + ChatColor.WHITE + "  Requires: Java " + DataModule.getJava() + " (or higher)");
+		sender().sendMessage(ChatColor.DARK_AQUA + "|______|_|  |_____|_|  |_|" + ChatColor.WHITE + "  Need support or have questions? https://discord.gg/Nvnrv3P");
 	}
 	public static final void info(final String message) {
-		getConsoleSender().sendMessage(ChatColor.WHITE + "[" + ChatColor.DARK_AQUA + "LTIM " + ChatColor.DARK_GREEN + "INFO" + ChatColor.WHITE + "] " + message);
+		sender().sendMessage(ChatColor.WHITE + "[" + ChatColor.DARK_AQUA + "LTIM " + ChatColor.DARK_GREEN + "INFO" + ChatColor.WHITE + "] " + message);
 	}
 	public static final void warning(final String message) {
-		getConsoleSender().sendMessage(ChatColor.WHITE + "[" + ChatColor.DARK_AQUA + "LTIM " + ChatColor.GOLD + "WARNING" + ChatColor.WHITE + "] " + ChatColor.YELLOW + message);
+		sender().sendMessage(ChatColor.WHITE + "[" + ChatColor.DARK_AQUA + "LTIM " + ChatColor.GOLD + "WARNING" + ChatColor.WHITE + "] " + ChatColor.YELLOW + message);
 	}
 	public static final void severe(final String message) {
-		getConsoleSender().sendMessage(ChatColor.WHITE + "[" + ChatColor.DARK_AQUA + "LTIM " + ChatColor.DARK_RED + "ERROR" + ChatColor.WHITE + "] " + ChatColor.RED + message);
+		sender().sendMessage(ChatColor.WHITE + "[" + ChatColor.DARK_AQUA + "LTIM " + ChatColor.DARK_RED + "ERROR" + ChatColor.WHITE + "] " + ChatColor.RED + message);
 	}
 	public static final void debug(final String message) {
-		getConsoleSender().sendMessage(ChatColor.DARK_GRAY + "[" + LTItemMail.class.getName() + "] " + message);
+		sender().sendMessage(ChatColor.DARK_GRAY + "[" + LTItemMail.class.getName() + "] " + message);
 	}
 	public static final void mailbox(final String message) {
-		getConsoleSender().sendMessage(ChatColor.WHITE + "[Mailbox-Log-Worker] " + message);
+		sender().sendMessage(ChatColor.WHITE + "[Mailbox-Log-Worker] " + message);
 	}
 	public static final void server(final List<String> warnings) {
-		getConsoleSender().sendMessage(ChatColor.WHITE + "========== " + ChatColor.DARK_AQUA + "LT Item Mail " + ChatColor.RED + "Version Control" + ChatColor.WHITE + " ==========");
-		for(final String warning: warnings) getConsoleSender().sendMessage(ChatColor.WHITE + warning);
-		getConsoleSender().sendMessage(ChatColor.WHITE + "==================================================");
+		sender().sendMessage(ChatColor.WHITE + "========== " + ChatColor.DARK_AQUA + "LT Item Mail " + ChatColor.RED + "Version Control" + ChatColor.WHITE + " ==========");
+		for(final String warning: warnings) sender().sendMessage(ChatColor.WHITE + warning);
+		sender().sendMessage(ChatColor.WHITE + "==================================================");
 	}
 	public static final void server(final Map<Integer, Map<String, Map<String, List<String>>>> messages) {
-		getConsoleSender().sendMessage(ChatColor.WHITE + "========== " + ChatColor.DARK_AQUA + "LT Item Mail " + ChatColor.LIGHT_PURPLE + "Messages Board" + ChatColor.WHITE + " ==========");
-		getConsoleSender().sendMessage("");
+		sender().sendMessage(ChatColor.WHITE + "========== " + ChatColor.DARK_AQUA + "LT Item Mail " + ChatColor.LIGHT_PURPLE + "Messages Board" + ChatColor.WHITE + " ==========");
+		sender().sendMessage("");
 		for(final Integer id : messages.keySet()) {
 			final Map<String, Map<String, List<String>>> contents1 = messages.get(id);
 			for(final String title : contents1.keySet()) {
 				final Map<String, List<String>> contents2 = contents1.get(title);
 				for(final String datetime : contents2.keySet()) {
-					getConsoleSender().sendMessage(ChatColor.WHITE + "[" + datetime + " | " + title + "]");
-					for(final String message : contents2.get(datetime)) getConsoleSender().sendMessage(ChatColor.WHITE + "  " + message);
+					sender().sendMessage(ChatColor.WHITE + "[" + datetime + " | " + title + "]");
+					for(final String message : contents2.get(datetime)) sender().sendMessage(ChatColor.WHITE + "  " + message);
 				}
-				getConsoleSender().sendMessage("");
+				sender().sendMessage("");
 			}
 		}
-		getConsoleSender().sendMessage(ChatColor.WHITE + "=================================================");
+		sender().sendMessage(ChatColor.WHITE + "=================================================");
 		for(final Player player : Bukkit.getOnlinePlayers()) if(PermissionModule.hasPermission(player, PermissionModule.Type.CMD_ADMIN_NOTIFY)) {
 			player.sendMessage(ChatColor.WHITE + "========== " + ChatColor.DARK_AQUA + "LT Item Mail " + ChatColor.LIGHT_PURPLE + "Messages Board" + ChatColor.WHITE + " ==========");
 			player.sendMessage("");
