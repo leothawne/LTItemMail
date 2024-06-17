3a96cc78501c55ac1a37f2371f5c6b37e7814c9c
BuildTools <31146142+leothawne@users.noreply.github.com>
2024-06-17 03:55:57 -0300

    Update DataModule.java

--- src/main/java/io/github/leothawne/LTItemMail/module/DataModule.java
+++ src/main/java/io/github/leothawne/LTItemMail/module/DataModule.java
@@ -5,20 +5,25 @@ import java.util.HashMap;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
+import io.github.leothawne.LTItemMail.LTItemMail;
+
 public final class DataModule {
 	private DataModule() {}
-	private static final String PROJECT_PAGES = "BUKKIT_DEV:dev.bukkit.org/projects/lt-item-mail,SPIGOT_MC:www.spigotmc.org/resources/62294";
-	private static final String VERSIONS = "DATABASE:5,"
+	private static final String PROJECT_PAGES = ""
+			+ "BUKKIT_DEV:dev.bukkit.org/projects/lt-item-mail,"
+			+ "SPIGOT_MC:www.spigotmc.org/resources/62294";
+	private static final String VERSIONS = ""
+			+ "DATABASE:4,"
 			+ "CONFIG_YML:11,"
 			+ "ENGLISH_YML:9,"
 			+ "PORTUGUESE_YML:9,"
-			+ "VIETNAMESE_YML:3,"
-			+ "MINECRAFT:1.14,"
-			+ "JAVA:8";
-	private static final String Plugin_Date = "21/03/2024 19:08 (BRT)";
-	private static final String Minecraft_Build = "1.13-R0.1-SNAPSHOT";
-	private static final String Update_URL = "https://leothawne.github.io/LTItemMail/api/latest.html";
-	private static final String Plugin_URL = "https://leothawne.github.io/LTItemMail/api/$version/plugin.yml";
+			+ "VIETNAMESE_YML:3";
+	private static final String DATE = "21/03/2024 19:08 (BRT)";
+	private static final String API = "1.14-R0.1-SNAPSHOT";
+	private static final String JAVA = "1.8";
+	private static final String UPDATE = "https://leothawne.github.io/LTItemMail/api/latest.html";
+	private static final String PLUGIN = "https://leothawne.github.io/LTItemMail/api/$version/plugin.yml";
+	private static final String RESOURCE = "https://github.com/leothawne/LTItemMail-ResourcePack/releases/latest/download/LTItemMail-ResourcePack.zip";
 	public static final String getProjectPage(final ProjectType type) {
 		final String[] pageString = PROJECT_PAGES.split(",");
 		final HashMap<ProjectType, String> pageMap = new HashMap<>();
@@ -37,30 +42,29 @@ public final class DataModule {
 		}
 		return versionMap.get(type);
 	}
-	public static final String getVersionDate() {
-		return Plugin_Date;
+	public static final String getDate() {
+		return DATE;
 	}
-	public static final String getMinecraftBuild() {
-		return Minecraft_Build;
+	public static final String getJava() {
+		return JAVA;
 	}
-	public static final String getUpdateURL() {
-		return Update_URL;
+	public static final String getUpdatePath() {
+		return UPDATE;
 	}
-	public static final String getPluginURL(final String version) {
-		String Plugin_URL = DataModule.Plugin_URL;
-		Plugin_URL = Plugin_URL.replace("$version", version);
-		return Plugin_URL;
+	public static final String getPluginPath(final String version) {
+		return PLUGIN.replace("$version", version);
+	}
+	public static final String getResourcePack() {
+		return RESOURCE;
 	}
 	public static final void showVersion(final String version, final CommandSender sender) {
-		sender.sendMessage(ChatColor.YELLOW + "Version " + ChatColor.GREEN + version + ChatColor.YELLOW + " (" + ChatColor.GREEN + Plugin_Date + ChatColor.YELLOW + "), Minecraft " + ChatColor.GREEN + DataModule.getVersion(VersionType.MINECRAFT) + ChatColor.YELLOW + " (Java " + ChatColor.GREEN + DataModule.getVersion(VersionType.JAVA) + ChatColor.YELLOW + ", build " + ChatColor.GREEN + Minecraft_Build + ChatColor.YELLOW + ").");
+		sender.sendMessage(ChatColor.YELLOW + LTItemMail.getInstance().getDescription().getName() + ": " + ChatColor.GREEN + version + ChatColor.YELLOW + ", Spigot API: " + ChatColor.GREEN + API + ChatColor.YELLOW + ", Java: " + ChatColor.GREEN + JAVA);
 	}
 	public enum VersionType {
 		CONFIG_YML,
 		ENGLISH_YML,
 		PORTUGUESE_YML,
 		VIETNAMESE_YML,
-		MINECRAFT,
-		JAVA,
 		DATABASE
 	}
 	public enum ProjectType {
