1025e55fecfe1661d459639581e668aeea73c771
BuildTools <31146142+leothawne@users.noreply.github.com>
2025-01-25 06:31:35 -0300

    Permissions auto registration

--- src/main/java/br/net/gmj/nobookie/LTItemMail/module/PermissionModule.java
+++ src/main/java/br/net/gmj/nobookie/LTItemMail/module/PermissionModule.java
@@ -1,50 +1,81 @@
 package br.net.gmj.nobookie.LTItemMail.module;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
 
 import br.net.gmj.nobookie.LTItemMail.module.ext.LTVault;
 
 public class PermissionModule {
 	private PermissionModule() {}
+	public static final void register() {
+		final List<Type> player = new ArrayList<>();
+		final List<Type> admin = new ArrayList<>();
+		for(final Type perm : Type.values()) if(!perm.equals(Type.CMD_PLAYER_ALL) && !perm.equals(Type.CMD_ADMIN_ALL)) {
+			if(perm.name().startsWith("CMD_PLAYER_") && perm.name().startsWith("BLOCK_PLAYER_")) player.add(perm);
+			if(perm.name().startsWith("CMD_ADMIN_") && perm.name().startsWith("BLOCK_ADMIN_")) admin.add(perm);
+			final Permission permission = new Permission(perm.node(), perm.permissionDefault());
+			Bukkit.getPluginManager().addPermission(permission);
+		}
+		final Permission player_all = new Permission(Type.CMD_PLAYER_ALL.node(), Type.CMD_PLAYER_ALL.permissionDefault());
+		for(final Type perm : player) player_all.getChildren().put(perm.node(), true);
+		player_all.recalculatePermissibles();
+		Bukkit.getPluginManager().addPermission(player_all);
+		final Permission admin_all = new Permission(Type.CMD_ADMIN_ALL.node(), Type.CMD_ADMIN_ALL.permissionDefault());
+		for(final Type perm : admin) admin_all.getChildren().put(perm.node(), true);
+		admin_all.recalculatePermissibles();
+		Bukkit.getPluginManager().addPermission(admin_all);
+	}
 	public static final boolean hasPermission(final CommandSender sender, final Type permission) {
 		final String node = permission.node();
 		if(ExtensionModule.getInstance().isRegistered(ExtensionModule.Function.VAULT_PERMISSION)) return ((LTVault.Permission) ExtensionModule.getInstance().get(ExtensionModule.Function.VAULT_PERMISSION)).getAPI().has(sender, node);
 		return sender.hasPermission(node);
 	}
 	public enum Type {
-		CMD_PLAYER_MAIN("ltitemmail.player"),
-		CMD_PLAYER_VERSION("ltitemmail.player.version"),
-		CMD_PLAYER_LIST("ltitemmail.player.list"),
-		CMD_PLAYER_COLOR("ltitemmail.player.color"),
-		CMD_PLAYER_OPEN("ltitemmail.player.open"),
-		CMD_PLAYER_DELETE("ltitemmail.player.delete"),
-		CMD_PLAYER_SEND("ltitemmail.player.send"),
-		CMD_PLAYER_PRICE("ltitemmail.player.price"),
-		CMD_PLAYER_INFO("ltitemmail.player.info"),
-		CMD_PLAYER_BLOCKS("ltitemmail.player.blocks"),
-		CMD_PLAYER_NOTIFY("ltitemmail.player.notify"),
-		CMD_ADMIN_MAIN("ltitemmail.admin"),
-		CMD_ADMIN_UPDATE("ltitemmail.admin.update"),
-		CMD_ADMIN_LIST("ltitemmail.admin.list"),
-		CMD_ADMIN_RECOVER("ltitemmail.admin.recover"),
-		CMD_ADMIN_BAN("ltitemmail.admin.ban"),
-		CMD_ADMIN_UNBAN("ltitemmail.admin.unban"),
-		CMD_ADMIN_BANLIST("ltitemmail.admin.banlist"),
-		CMD_ADMIN_INFO("ltitemmail.admin.info"),
-		CMD_ADMIN_RELOAD("ltitemmail.admin.reload"),
-		CMD_ADMIN_BLOCKS("ltitemmail.admin.blocks"),
-		CMD_ADMIN_NOTIFY("ltitemmail.admin.notify"),
-		CMD_ADMIN_BYPASS("ltitemmail.admin.bypass"),
-		BLOCK_PLAYER_PLACE("ltitemmail.block.place"),
-		BLOCK_PLAYER_BREAK("ltitemmail.block.break"),
-		BLOCK_PLAYER_USE("ltitemmail.block.use"),
-		BLOCK_ADMIN_BREAK("ltitemmail.block.break.bypass");
+		CMD_PLAYER_ALL("ltitemmail.player.all", "TRUE"),
+		CMD_PLAYER_MAIN("ltitemmail.player", "TRUE"),
+		CMD_PLAYER_VERSION("ltitemmail.player.version", "TRUE"),
+		CMD_PLAYER_LIST("ltitemmail.player.list", "TRUE"),
+		CMD_PLAYER_COLOR("ltitemmail.player.color", "TRUE"),
+		CMD_PLAYER_OPEN("ltitemmail.player.open", "TRUE"),
+		CMD_PLAYER_DELETE("ltitemmail.player.delete", "TRUE"),
+		CMD_PLAYER_SEND("ltitemmail.player.send", "TRUE"),
+		CMD_PLAYER_PRICE("ltitemmail.player.price", "TRUE"),
+		CMD_PLAYER_INFO("ltitemmail.player.info", "TRUE"),
+		CMD_PLAYER_BLOCKS("ltitemmail.player.blocks", "TRUE"),
+		CMD_PLAYER_NOTIFY("ltitemmail.player.notify", "TRUE"),
+		CMD_ADMIN_ALL("ltitemmail.admin.all", "OP"),
+		CMD_ADMIN_MAIN("ltitemmail.admin", "OP"),
+		CMD_ADMIN_UPDATE("ltitemmail.admin.update", "OP"),
+		CMD_ADMIN_LIST("ltitemmail.admin.list", "OP"),
+		CMD_ADMIN_RECOVER("ltitemmail.admin.recover", "OP"),
+		CMD_ADMIN_BAN("ltitemmail.admin.ban", "OP"),
+		CMD_ADMIN_UNBAN("ltitemmail.admin.unban", "OP"),
+		CMD_ADMIN_BANLIST("ltitemmail.admin.banlist", "OP"),
+		CMD_ADMIN_INFO("ltitemmail.admin.info", "OP"),
+		CMD_ADMIN_RELOAD("ltitemmail.admin.reload", "OP"),
+		CMD_ADMIN_BLOCKS("ltitemmail.admin.blocks", "OP"),
+		CMD_ADMIN_NOTIFY("ltitemmail.admin.notify", "OP"),
+		CMD_ADMIN_BYPASS("ltitemmail.admin.bypass", "OP"),
+		BLOCK_PLAYER_PLACE("ltitemmail.block.place", "TRUE"),
+		BLOCK_PLAYER_BREAK("ltitemmail.block.break", "TRUE"),
+		BLOCK_PLAYER_USE("ltitemmail.block.use", "TRUE"),
+		BLOCK_ADMIN_BREAK("ltitemmail.block.break.bypass", "OP");
 		private final String node;
-		Type(final String node){
+		private final PermissionDefault permissionDefault;
+		Type(final String node, final String permissionDefault){
 			this.node = node;
+			this.permissionDefault = PermissionDefault.getByName(permissionDefault);
 		}
 		public final String node() {
 			return node;
+		}
+		public final PermissionDefault permissionDefault() {
+			return permissionDefault;
 		}
 	}
 }
